17587d59384a0ee4f3bb8ec61c3464ca92862a9b
TaisyaFreelanse <smilikgirl1@gmail.com>
2025-11-15 11:47:39 +0200

    Refactor event management logic in index.vue; streamline event creation and editing processes, and improve validation for user inputs to enhance overall functionality.

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="padding: 40px;">
+      <h1 style="font-size:32px; margin-bottom:20px;">External API Playground</h1>
+  
+      <p>Выберите раздел:</p>
+  
+      <ul style="margin-top:20px; font-size:20px;">
+        <li><a href="/demo/api-register">Регистрация API ключа</a></li>
+        <li><a href="/demo/external-upload">Создание/загрузка ивентов</a></li>
+      </ul>
+    </div>
+  </template>
+  
